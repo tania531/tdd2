29fc71044b2c33f67b4a724c1c111cb26a746231
Tania <tania.dev77@gmail.com>
2015-06-11 14:44:23 -0700

    add first test case

---
+++ index.js
@@ -0,0 +1,11 @@
+var doubler = function(a,b){
+
+//return sum of doubles
+
+}
+
+console.assert(doubler(4,2) === 12);
+console.assert(doubler(1,3) === 8);
+console.assert(doubler(-1,3) === 4);
+
+console.assert(true);
